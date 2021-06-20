d4cc14061d0bbd9bd2d1db6c262e620cf8f9a105
Breck <breck@alias-i.com>
2021-06-20 10:45:19 -0400

    most of test framework done

--- R/evalPredictionsSIRTD.R
+++ R/evalPredictionsSIRTD.R
@@ -1,6 +1,20 @@
 library("cmdstanr")
 library(tidyverse)
 
+#Experiments
+# +- twitter
+# isomorphic model, compare internal ode
+# block head ode, trapezoidal, rk45 ODEs
+# 
+# Vary beta
+# Vary gamma
+# Vary contacts/Tweets
+# Recreate chaotic data
+
+# evaluation
+# Did the model fit? Rhat, ess, ...
+# % sim data outside 50% 80%, 90%, 95% for tweets/deaths
+
 paramsForUnitTest <- function(runDf, i, seed, nPop, nDays, nDailyContacts,
                              nPatientZero) {
    return(
@@ -29,24 +43,46 @@ paramsForUnitTest <- function(runDf, i, seed, nPop, nDays, nDailyContacts,
    )
 }
 
+countPredInInterval <- function(fitActualDf = fitActualDf, fitPredDf = fitPredDf, 
+                                maxQuantileLabel = maxQuantileLabel, 
+                                minQuantileLabel = minQuantileLabel) {
+  actualsCovered = 0
+  for (day in 1:nrow(fitActualDf)) {
+    if (fitActualDf[1,]$mean <= fitPredDf[day,][[maxQuantileLabel]] &&
+        fitActualDf[1,]$mean >= fitPredDf[day,][[minQuantileLabel]]) {
+      actualsCovered = actualsCovered + 1
+    }
+  }
+  return(actualsCovered)
+}
+
 source(here::here("R","SIRTDsim.R"))
 seed = 4614
 set.seed(seed)
 nPop <- 10000
 nWeeks <- 10
 nDays <- nWeeks * 7
-nSims <- 10
+nSims <- 2
 
 runDf <- data.frame(runId = c(1:nSims))
-runDf$gamma <- runif(nSims,0,.5) #c(1/2,1/10)
-runDf$tweetRate <-  runif(nSims,0,2) #c(.2,.8)
-runDf$deathRate <-  runif(nSims,.01,1) #c(.1,.2)
-runDf$daysToDeath <-  runif(nSims,1,20) #c(1,10)
+runDf$tweetsInInterval <- rep(NA, nSims)
+runDf$casesInInterval <- rep(NA, nSims)
+runDf$gamma <- runif(nSims, 0, .5) #c(1/2,1/10)
+runDf$tweetRate <-  runif(nSims, 0, 2) #c(.2,.8)
+runDf$deathRate <-  runif(nSims, .01, 1) #c(.1,.2)
+runDf$daysToDeath <-  runif(nSims, 1, 20) #c(1,10)
 runDf$sdBeta <-  rep(.00001, nSims) #c(.0001,.2)
-runDf$betaMean <-  runif(nSims,0,1) #c(.2,.4)
-runDf$nPatientZero <-  rep(10,nSims) #c(10,100)
-runDf$nDailyContactsInf <- rep(10,nSims)
-runDf$betaInfRatePerDay <- list(rep(NA,nDays),rep(NA,nDays))
+runDf$betaMean <-  runif(nSims, 0, 1) #c(.2,.4)
+runDf$nPatientZero <-  rep(10, nSims) #c(10,100)
+runDf$nDailyContactsInf <- rep(10, nSims)
+betaInfRatePerDayList <- list(rep(NA, nDays))
+runDf$betaInfRatePerDay <- rep(betaInfRatePerDayList, nSims)
+runDf$run_block_ODE <- rep(1, nSims)
+runDf$run_rk45_ODE <- rep(0, nSims)
+runDf$run_twitter <- rep(1, nSims)
+runDf$nPop <- rep(nPop, nSims)
+runDf$nDays <- rep(nDays, nSims)
+runDf$compute_likelihood <- rep(1, nSims)
 
 for (i in 1:nSims) {
   betaForWeek = abs(rnorm(nWeeks, runDf[i,]$betaMean, runDf[i,]$sdBeta))
@@ -61,8 +97,8 @@ for (i in 1:nSims) {
   }
   
   simDf = SirtdVaryBeta(runName = runDf[i,]$runId, 
-                        nPop = nPop, 
-                        nDays = nDays, 
+                        nPop = runDf[i,]$nPop, 
+                        nDays = runDf[i,]$nDays, 
                         print = TRUE,
                         betaDailyInfectionRates = unlist(runDf[i,]$betaInfRatePerDay),
                         gammaResolvedPerDayRate = runDf[i,]$gamma,
@@ -77,23 +113,23 @@ for (i in 1:nSims) {
                  nrow = nrow(simDf), ncol = 5)
 	compartment = 4
   tweetSourceIndex <- 2
-  stan_data <- list(n_days = nrow(simDf),
-                  y0 = c(nPop - runDf[i,]$nPatientZero, runDf[i,]$nPatientZero, 0, 0),
+  stan_data <- list(n_days = runDf[i,]$nDays,
+                  y0 = c(runDf[i,]$nPop - runDf[i,]$nPatientZero, runDf[i,]$nPatientZero, 0, 0),
                   t0 = 0,
-                  ts = 1:nDays,
-                  N = nPop,
+                  ts = 1:runDf[i,]$nDays,
+                  N =runDf[i,]$ nPop,
                   n_compartments = ncol(simData) - 1,
                   nDataCols = ncol(simData),
                   compartmentDays = simData,
                   compartment = compartment,
                   tweetIndex = 5,
                   tweetSourceIndex = tweetSourceIndex,
-                  compute_likelihood = 1,
-                  run_twitter = 1,
-                  run_SIR = 1,
-                  check_ODE = 0)
+                  compute_likelihood = runDf[i,]$compute_likelihood,
+                  run_twitter = runDf[i,]$run_twitter,
+                  run_block_ODE = runDf[i,]$run_block_ODE,
+                  run_rk45_ODE =runDf[i,]$run_rk45_ODE)
 
-		  model <- cmdstan_model(here::here("stan", "tweet_sird_negbin.stan"))
+		  model <- cmdstan_model(here::here("stan", "tweet_sird.stan"))
 
       fit <- model$sample(data=stan_data,
                     parallel_chains = 4,
@@ -101,6 +137,25 @@ for (i in 1:nSims) {
                     iter_sampling = 1000,
                     chains = 4,
                     seed = 4857)
+      
+      # stan_data_2 <- list(n_days = nrow(simDf),
+      #                   y0 = c(nPop - runDf[i,]$nPatientZero, runDf[i,]$nPatientZero, 0, 0, 0), # one more zero here
+      #                   t0 = 0,
+      #                   ts = 1:nDays,
+      #                   compute_likelihood = 1,
+      #                   use_twitter = 1, # it is now use_twitter
+      #                   death_count = simDf$d,
+      #                   symptomaticTweets = simDf$tweets,
+      #                   trapezoidal_solver = 0)
+      # 
+      # model2 <- cmdstan_model(here::here("stan", "tweet_sird_negbin_optimized.stan"))
+      # 
+      # fit2 <- model2$sample(data=stan_data_2,
+      #                     parallel_chains = 4,
+      #                     iter_warmup = 1000,
+      #                     iter_sampling = 1000,
+      #                     chains = 4,
+      #                     seed = 4857)
       
       minQuantile = .2
       maxQuantile = .8
@@ -115,10 +170,27 @@ for (i in 1:nSims) {
                                  ~quantile(.x, probs = c(minQuantile, maxQuantile)))
       predTweetsDf$day = 1:nrow(predTweetsDf)
       
+      runDf[i,]$casesInInterval = countPredInInterval(fitActualDf = 
+                                           fit$summary(variables = 
+                                                         c('actual_cases'), mean),
+                                         fitPredDf = predCasesDf,
+                                         maxQuantileLabel = maxQuantileLabel,
+                                         minQuantileLabel = minQuantileLabel)
+      
+      runDf[i,]$tweetsInInterval = countPredInInterval(fitActualDf = 
+                                            fit$summary(variables = 
+                                                          c('actual_tweets'), mean),
+                                         fitPredDf = predTweetsDf,
+                                         maxQuantileLabel = maxQuantileLabel,
+                                         minQuantileLabel = minQuantileLabel)
       
       predCasesLine = 'predicted cases'
-      predCasesRibbon = paste0('predicted cases ',minQuantileLabel, '/', maxQuantileLabel)
+      predCasesRibbon = paste0('predicted cases ',minQuantileLabel, '/', 
+                               maxQuantileLabel)
+      print(sprintf("Run %d predicted %d cases and %d tweets within interval",
+            i, runDf[i,]$casesInInterval, runDf[i,]$tweetsInInterval))
       
+      if (FALSE) {
       plot = ggplot(data = NULL, aes(x = day, y = mean)) +
         #cases no twitter info
         geom_ribbon(data = predCasesDf, aes(ymin = .data[[minQuantileLabel]], 
@@ -147,6 +219,6 @@ for (i in 1:nSims) {
              caption = summary) +
         theme(plot.caption=element_text(size=12, hjust=0, margin=margin(15,0,0,0)))
       print(plot)
-        
+      }        
 }
     
