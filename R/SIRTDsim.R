4f944fc1d82c436f5c2b9beb716c66772b8cf010
Breck <breck@alias-i.com>
2021-06-29 22:22:22 -0400

    update from my branch that got a bit far off

--- R/SIRTDsim.R
+++ R/SIRTDsim.R
@@ -1,5 +1,6 @@
 SirtdVaryBeta <- function(runName = runName,
-                         nPop = nPop,
+                          seed = seed,
+                          nPop = nPop,
                          nDays = nDays,
                          print = PRINT,
                          betaDailyInfectionRates = betaDailyInfectionRates,
@@ -9,7 +10,7 @@ SirtdVaryBeta <- function(runName = runName,
                          nPatientZero = nPatientZero,
                          meanDaysToDeathFromT = meanDaysToDeathFromT,
                          nDailyContacts = nDailyContacts) {
-
+  set.seed(seed)
   dayState = c(rep('i',nPatientZero), rep('s', nPop - nPatientZero))
   tweets = rep(0, nPop)
   colNames = c('runName', 'day', 's', 'i', 'r', 't', 'd_today', 'd', 'tweets')
@@ -23,7 +24,6 @@ SirtdVaryBeta <- function(runName = runName,
     df[day,]$i = length(subset(dayState, dayState == 'i'))
     df[day,]$r = length(subset(dayState, dayState == 'r'))
     df[day,]$t = length(subset(dayState, dayState == 't'))
-    df[day,]$d_today = length(subset(dayState, dayState == 'd_today'))
     df[day,]$d = length(subset(dayState, dayState == 'd'))
     df[day,]$day = day
     df[day,]$tweets = sum(tweets)
@@ -31,15 +31,16 @@ SirtdVaryBeta <- function(runName = runName,
       cat(
         sprintf(
           "Day=%d, susceptible=%d, infected=%d, recovered=%d, terminal=%d,
-          dead today=%d, dead=%d, tweets=%d, R0=%.2f\n",
-          df[day,]$day, df[day,]$s, df[day,]$i, df[day,]$r, df[day,]$t, df[day,]$d_today,
+           dead=%d, tweets=%d, R0=%.2f\n",
+          df[day,]$day, df[day,]$s, df[day,]$i, df[day,]$r, df[day,]$t,
           df[day,]$d, df[day,]$tweets, betaDailyInfectionRates[day]/gammaResolvedPerDayRate))
     }
+
     tweets = rep(0, nPop) #start fresh every day, certainly wrong.
     for (per in 1:nPop) {
       #end infectious period
       if (dayState[per] == 'i') {
-        tweets[per] = as.integer(rbernoulli(1, tweetRateInfected))
+        tweets[per] = rpois(1, tweetRateInfected)
         if (rbinom(n = 1, size = 1, prob = gammaResolvedPerDayRate) == 1) {
           if (rbinom(n = 1, size = 1, prob = deathRate) == 1) {
             nextDayState[per] = 't'
@@ -50,6 +51,7 @@ SirtdVaryBeta <- function(runName = runName,
         }
       }
       if (dayState[per] == 't') {
+            
         if (rbinom(n = 1, size =1, prob = 1/meanDaysToDeathFromT) == 1) {
           nextDayState[per] = 'd'
         }
@@ -57,11 +59,9 @@ SirtdVaryBeta <- function(runName = runName,
     }
     for (per in 1:nPop) {
       if (dayState[per] == 'i') {
-        for (otherPer in sample(1:nPop, nDailyContacts)) {
-          if (dayState[otherPer] == 's' &&
-              rbinom(n = 1, size = 1,
-                     prob = min(betaDailyInfectionRates[day],1.0)) == 1) {
-            nextDayState[otherPer] = 'i'
+        for (otherPer in sample(1:nPop, rpois(n = 1, lambda = betaDailyInfectionRates[day]))) {
+          if (dayState[otherPer] == 's') {  #subtle but will reduce infs if there are not lots of s
+              nextDayState[otherPer] = 'i'
           }
         }
       }
