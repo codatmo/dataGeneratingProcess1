dc49ef297a4b8d6611a1ff4afa2392245d9b515f
Jose Storopoli <thestoropoli@gmail.com>
2021-07-30 11:54:53 -0300

    tweet_sirtd_negbin_ODE model inside runEval.R

--- R/runEval.R
+++ R/runEval.R
@@ -13,7 +13,7 @@ source(here::here("R","modeling_configs.R"))
 run_df <- setup_run_df(seed = 93435, n_pop = 1000, n_days = 7) # in R/util.R
 run_df <- sim_brazil_1(run_df) # in R/sim_configs.R
 draws_run_df <- sim_draw_params(2, run_df) # in R/sim_configs.R
-run_df <- rbind(run_df, draws_run_df) 
+run_df <- rbind(run_df, draws_run_df)
 
 run_df <- model_stan_SIRDs(run_df) #in R/modeling_configs.R
 run_df$ode_solver <- 'block' # set ODE solver in Stan program across all runs
@@ -24,12 +24,12 @@ run_df$reports <- list(c('graph_sim','graph_ODE', 'graph_tweets', 'graph_d', 'pl
 for (j in 1:nrow(run_df)) {
   fit <- NA
   if (run_df[j,]$model_to_run == 'twitter_sird') {
-    stan_data <- 
+    stan_data <-
       list(n_days = run_df[j,]$n_days,
            sDay1 = run_df[j,]$n_pop - 1,
            iDay1 = 1,
            rDay1 = 0,
-           dDay1 = 0,            
+           dDay1 = 0,
            NPop = run_df[j,]$n_pop,
            tweets = unlist(run_df[j,]$tweets),
            deaths = unlist(run_df[j,]$d),
@@ -38,7 +38,7 @@ for (j in 1:nrow(run_df)) {
            run_block_ODE = ifelse(run_df[j,]$ode_solver == 'block', 1, 0),
            run_rk45_ODE = ifelse(run_df[j,]$ode_solver == 'rk45', 1, 0))
     model <- cmdstan_model(here::here("stan", "tweet_sird.stan"))
-    
+
     fit <- model$sample(data=stan_data,
                         parallel_chains = 4,
                         iter_warmup = 1000,
@@ -47,18 +47,27 @@ for (j in 1:nrow(run_df)) {
                         seed = 4857)
     run_df[j,]$fit = list(fit)
   }
-  else if (run_df[j,]$model_to_run == 'tweet_sird_negbin_optimized') {
+  else if (run_df[j,]$model_to_run == 'tweet_sirtd_negbin_ODE') {
     stan_data_2 <- list(n_days = run_df[j,]$n_days,
-                        y0 = c(run_df[j,]$n_pop - run_df[j,]$n_patient_zero, 
+                        y0 = c(run_df[j,]$n_pop - run_df[j,]$n_patient_zero,
                                run_df[j,]$n_patient_zero, 0, 0, 0), # one more zero here
                         t0 = 0,
                         ts = 1:run_df[j,]$n_days,
                         compute_likelihood = run_df[j,]$compute_likelihood,
                         use_twitter = run_df[j,]$apply_twitter_data,
-                        death_count = run_df[j,]$d,
-                        symptomaticTweets = run_df[j,]$tweets,
-                        trapezoidal_solver = 0)
-    model2 <- cmdstan_model(here::here("stan", "tweet_sird_negbin_optimized.stan"))
+                        death_count = unlist(run_df[j,]$d),
+                        symptomaticTweets = unlist(run_df[j,]$tweets),
+                        prior_beta_mean = 0,
+                        prior_beta_std = 10,
+                        prior_omega_mean = 0,
+                        prior_omega_std = 10,
+                        prior_dI_mean = 0,
+                        prior_dI_std = 10,
+                        prior_dT_mean = 0,
+                        prior_dT_std = 10,
+                        prior_twitter_lambda = 1
+                        )
+    model2 <- cmdstan_model(here::here("stan", "tweet_sirtd_negbin_ODE.stan"))
     fit <- model2$sample(data=stan_data_2,
                          parallel_chains = 4,
                          iter_warmup = 1000,
@@ -67,9 +76,9 @@ for (j in 1:nrow(run_df)) {
                          seed = 4857)
   }
   if (run_df[j,]$model_to_run != 'none') {
-    d_tweets_in_interval = countPredictionsInQuantile(fit = fit, 
-                                                      run_df = run_df, 
-                                                      j = j, print = TRUE)  
+    d_tweets_in_interval = countPredictionsInQuantile(fit = fit,
+                                                      run_df = run_df,
+                                                      j = j, print = TRUE)
     run_df[j,]$d_in_interval = d_tweets_in_interval[1]
     run_df[j,]$tweets_in_interval = d_tweets_in_interval[2]
   }
@@ -86,13 +95,13 @@ for (j in 1:nrow(run_df)) {
     plot <- plot + graph_ODE(data_df = run_df[j,], fit = fit, hide_s = TRUE)
   }
   if ('graph_tweets' %in% unlist(run_df[j,]$reports)) {
-    plot <- plot_predictions(plot = plot, prediction_label = 'pred_tweets', 
-                             fit = fit, 
+    plot <- plot_predictions(plot = plot, prediction_label = 'pred_tweets',
+                             fit = fit,
                              show_ribbon = TRUE)
   }
   if ('graph_d' %in% unlist(run_df[j,]$reports)) {
-    plot <- plot_predictions(plot = plot, prediction_label = 'pred_deaths', 
-                             fit = fit, 
+    plot <- plot_predictions(plot = plot, prediction_label = 'pred_deaths',
+                             fit = fit,
                              show_ribbon = TRUE)
   }
   if ('plot' %in% unlist(run_df[j,]$reports)) {
